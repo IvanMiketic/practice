015b3749cad4c1b25a7ed42e7f00ebda7ff9c2b3
ivan miketic <ivmiketic@gmail.com>
2017-04-17 14:34:02 -0400

    added index view and controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -9,8 +9,18 @@ class ArticlesController < ApplicationController
 		redirect_to @article
 	end
 
+ 	def index
+		@articles = Article.all
+	end
+
+	def show 
+		@article = Article.find(params[:id])
+	end
+	
 	private
+
 		def article_params
 			params.require(:article).permit(:title, :text)
 		end
+		
 end
