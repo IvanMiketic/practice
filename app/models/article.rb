17fe7ee2629039e2d29fb360bc56175596d655c2
ivan miketic <ivmiketic@gmail.com>
2017-04-21 16:11:36 -0400

    added comments

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,10 +1,6 @@
+
 class Article < ApplicationRecord
-	validates :title, presence: true
-
-	before_save :check_title
-
-
-	def check_title
-		return title.length < 20
-	end
+  has_many :comments
+  validates :title, presence: true,
+                    length: { minimum: 5 }
 end
